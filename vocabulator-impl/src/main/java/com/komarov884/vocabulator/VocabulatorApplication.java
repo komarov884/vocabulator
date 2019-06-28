d63121c897fe2779a1d7bee6ee397f843229bb30
Vasilii_Komarov <vasilii_komarov@epam.com>
2019-06-28 16:37:11 +0400

    Initial commit

---
+++ vocabulator-impl/src/main/java/com/komarov884/vocabulator/VocabulatorApplication.java
@@ -0,0 +1,18 @@
+package com.komarov884.vocabulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * VocabulatorApplication.
+ *
+ * @author Vasilii_Komarov
+ */
+@SpringBootApplication
+public class VocabulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocabulatorApplication.class);
+    }
+
+}
